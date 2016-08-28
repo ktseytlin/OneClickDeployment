7fc73144a3995d4abd3802ece53d1e8a8798060a
Keren Tseytlin <ktseytlin@users.noreply.github.com>
2016-08-28 16:26:59 -0400

    Update createEC2.py

--- createEC2.py
+++ createEC2.py
@@ -7,38 +7,37 @@ if len(sys.argv) < 3:
     print "============================\n"
     sys.exit()
 
+print('Loading function')
+
+# Check if even need to pass in the keys if running the function from my public VPC, might not be necessary
 client = boto3.client(
   'ec2',
   aws_access_key_id=sys.argv[1],
   aws_secret_access_key=sys.argv[2]
   )
 
-ec2 = boto3.resource('ec2')
-instance = ec2.Instance('oneclick')
+def lambda_handler(event, context):
+    ec2 = boto3.resource('ec2')
+    instance = ec2.Instance('oneclick')
 
-ec2.create_instances(
-  ImageId='ami-fe5b6790', 
-  MinCount=1,
-  MaxCount=1,
-  KeyName='',
-  SecurityGroups=['',],
-  UserData=user_data,
-  InstanceType='t1.micro',
-  IamInstanceProfile={
-    'ARN': '',
-    'Name': ''
-    },
-  EbsOptimized=False,
-  Monitoring=False
-  )
+    ec2.create_instances(
+        ImageId='ami-2051294a', 
+        MinCount=1,
+        MaxCount=1,
+        KeyName='keren-publicAWS.pem',
+        UserData=user_data,
+        InstanceType='t1.micro',
+        EbsOptimized=False,
+        Monitoring=False
+    )
 
 
-user_data = (
-  #!/bin/bash
-  git clone https://github.com/ktseytlin/OneClickDeployment.git
-  cd reactServer
-  # download dependencies
-  npm install
-  # start server
-  npm run start
-  )
+    user_data = (
+        #!/bin/bash
+        git clone https://github.com/ktseytlin/OneClickDeployment.git
+        cd reactServer
+        # download dependencies
+        npm install
+        # start server
+        npm run start
+    )

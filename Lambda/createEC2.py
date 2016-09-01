dbbb1355688bc5fdc984bab9c421d4dc491d5852
Keren Tseytlin <keren.tseytlin@capitalone.com>
2016-09-01 11:28:43 -0400

    update lambda function

--- Lambda/createEC2.py
+++ Lambda/createEC2.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python
+
+# http://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.Subnet.create_instances
+
 import boto3
+import logging
 
-if len(sys.argv) < 3:
-    print "\n============================"
-    print "Error: Need to pass in AWS keys."
-    print "============================\n"
-    sys.exit()
+# if len(sys.argv) < 3:
+#     print "\n============================"
+#     print "Error: Need to pass in AWS keys."
+#     print "============================\n"
+#     sys.exit()
 
-print('Loading function')
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.info('Loading function')
 
 # Check if even need to pass in the keys if running the function from my public VPC, might not be necessary
-client = boto3.client(
-    'ec2',
-    aws_access_key_id=sys.argv[1],
-    aws_secret_access_key=sys.argv[2]
-    )
+# client = boto3.client(
+#     'ec2',
+#     aws_access_key_id=sys.argv[1],
+#     aws_secret_access_key=sys.argv[2]
+#     )
+
+ec2 = boto3.resource('ec2')
 
 def lambda_handler(event, context):
-    ec2 = boto3.resource('ec2')
+    logger.info('In lambda function')
     instance = ec2.Instance('oneclick')
 
     ec2.create_instances(
@@ -40,3 +48,5 @@ def lambda_handler(event, context):
         # start server
         npm run start
     """)
+
+    logger.info('End of lambda function')
